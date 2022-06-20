1c5cffab51382dbde3c8bb4625e2ab07f6d12f61
jon6fingrs <53415122+jon6fingrs@users.noreply.github.com>
2022-06-20 18:56:03 -0400

    Add files via upload

--- run.sh
+++ run.sh
@@ -6,9 +6,9 @@ chown ${PUID} -R /ssl
 chown ${PUID} -R /mail
 chmod 644 -R /ssl
 chmod 644 -R /mail
-#sed -i "s/{ssl_cert}/${ssl_cert}/g" /etc/dovecot/conf.d/10-ssl.conf
-#sed -i "s/{ssl_key}/${ssl_key}/g" /etc/dovecot/conf.d/10-ssl.conf
-#sed -i "s/{ssl}/${ssl}/g" /etc/dovecot/conf.d/10-ssl.conf
-#sed -i "s/{disable_plaintext_auth}/${disable_plaintext_auth}/g" /etc/dovecot/conf.d/10-auth.conf
-#sed -i "s/{server_address}/${server_address}/g" /usr/share/dovecot/dovecot-openssl.cnf
+sed -i "s/{ssl_cert}/${ssl_cert}/g" /etc/dovecot/conf.d/10-ssl.conf
+sed -i "s/{ssl_key}/${ssl_key}/g" /etc/dovecot/conf.d/10-ssl.conf
+sed -i "s/{ssl}/${ssl}/g" /etc/dovecot/conf.d/10-ssl.conf
+sed -i "s/{disable_plaintext_auth}/${disable_plaintext_auth}/g" /etc/dovecot/conf.d/10-auth.conf
+sed -i "s/{server_address}/${server_address}/g" /usr/share/dovecot/dovecot-openssl.cnf
 dovecot -F
