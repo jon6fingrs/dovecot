d55c19b49818a27e124fcc6fb399b62967c9265f
jon6fingrs <katzjs@gmail.com>
2025-07-29 07:11:19 -0400

    Upgrade to Dovecot 2.4 - beta branch

--- run.sh
+++ run.sh
@@ -1,20 +1,46 @@
 #!/bin/bash
+set -e
 
-if id ${PUID} &>/dev/null; then
-  usermod -u ${PUID} ${email_username}
-  usermod -a -G users,sudo ${email_username}
-  mkhomedir_helper ${email_username}
+# --- USER SETUP ---
+# Check if someone already has the desired UID
+existing_user_with_uid=$(getent passwd "${PUID}" | cut -d: -f1)
+
+if id "${email_username}" &>/dev/null; then
+  # email_username exists
+  current_uid=$(id -u "${email_username}")
+  if [[ "${current_uid}" != "${PUID}" ]]; then
+    echo "Changing UID of '${email_username}' from ${current_uid} to ${PUID}"
+    usermod -u "${PUID}" "${email_username}"
+  fi
 else
-  useradd -m -u ${PUID} -G users,sudo -g users ${email_username}
+  if [[ -n "${existing_user_with_uid}" && "${existing_user_with_uid}" != "${email_username}" ]]; then
+    echo "Renaming user '${existing_user_with_uid}' to '${email_username}' to take over UID ${PUID}"
+    usermod -l "${email_username}" "${existing_user_with_uid}"
+    usermod -d "/home/${email_username}" -m "${email_username}"
+  else
+    echo "Creating user '${email_username}' with UID ${PUID}"
+    useradd -m -u "${PUID}" -g users -G sudo "${email_username}"
+  fi
 fi
 
+# Ensure group membership
+for group in users sudo; do
+  if ! id -nG "${email_username}" | grep -qw "$group"; then
+    usermod -a -G "$group" "${email_username}"
+  fi
+done
+
+# Set password
 echo "${email_username}:${email_password}" | chpasswd
-chown ${PUID} -R /ssl
-chown ${PUID} -R /mail
+
+# --- FILE PERMISSIONS ---
+chown "${PUID}" -R /ssl
+chown "${PUID}" -R /mail
 chmod 774 -R /ssl
 chmod 774 -R /mail
 
-if [[ $disable_plaintext_auth != "yes" ]]; then export disable_plaintext_auth=no; fi
+# --- SSL CONFIG ---
+if [[ $auth_allow_cleartext != "yes" ]]; then export auth_allow_cleartext=no; fi
 if [[ $ssl != "yes" && $ssl !=  "required" ]]
 then
   export ssl=no
@@ -22,21 +48,27 @@ then
   sed -i "s/ssl_key = </#ssl_key = </g" /etc/dovecot/conf.d/10-ssl.conf
 fi
 
+# --- SUBSTITUTIONS ---
 sed -i "s/{ssl_cert}/${ssl_cert}/g" /etc/dovecot/conf.d/10-ssl.conf
 sed -i "s/{ssl_key}/${ssl_key}/g" /etc/dovecot/conf.d/10-ssl.conf
 sed -i "s/{ssl}/${ssl}/g" /etc/dovecot/conf.d/10-ssl.conf
-sed -i "s/{disable_plaintext_auth}/${disable_plaintext_auth}/g" /etc/dovecot/conf.d/10-auth.conf
+sed -i "s/{auth_allow_cleartext}/${auth_allow_cleartext}/g" /etc/dovecot/conf.d/10-auth.conf
 sed -i "s/{server_address}/${server_address}/g" /usr/share/dovecot/dovecot-openssl.cnf
+
+# --- DH PARAM ---
 if [[ $dh_pem = "yes" ]]
 then
   if test -f "/etc/dovecot/dh.pem"
   then
     echo "dh.pem file exists!"
   else
-    echo "Creating dh.pem file"
+    echo "Creating dh.pem file."
+    echo "This will take a while."
     openssl dhparam 4096 > /etc/dovecot/dh.pem
   fi
 else
-  sed -i "s/ssl_dh = </#ssl_dh = </g" /etc/dovecot/conf.d/10-ssl.conf
+  sed -i "s/ssl_server_dh_file = /#ssl_server_dh_file = /g" /etc/dovecot/conf.d/10-ssl.conf
 fi
+
+# --- START DOVECOT ---
 dovecot -F
